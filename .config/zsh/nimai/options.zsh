25a4fe09b5530614d7d6fcfe6740d0becd721b94
nimai <nimai.m.patel@gmail.com>
2021-01-17 19:01:01 +0530

    added colors for man pages

--- .config/zsh/nimai/options.zsh
+++ .config/zsh/nimai/options.zsh
@@ -11,3 +11,16 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' 'r:|[._-]=* r:|=*' 'l:|=* r:
 # History in cache directory:
 HISTSIZE=10000000
 SAVEHIST=10000000
+
+function man() {
+	env \
+		LESS_TERMCAP_md=$(tput bold; tput setaf 2) \
+		LESS_TERMCAP_me=$(tput sgr0) \
+		LESS_TERMCAP_mb=$(tput blink) \
+		LESS_TERMCAP_us=$(tput setaf 4) \
+		LESS_TERMCAP_ue=$(tput sgr0) \
+		LESS_TERMCAP_so=$(tput smso) \
+		LESS_TERMCAP_se=$(tput rmso) \
+		PAGER="${commands[less]:-$PAGER}" \
+		man "$@"
+}
