291b3fa303f192e1fb8e97e794819221acdf33ef
nimai <nimai.m.patel@gmail.com>
2021-01-22 13:58:43 +0530

    moonfly

--- .config/nvim/lua/nimai/theme.lua
+++ .config/nvim/lua/nimai/theme.lua
@@ -1,15 +1,15 @@
 vim.o.termguicolors = true
 vim.o.background = 'dark'
-require('colorbuddy').colorscheme('gruvbox')
+vim.cmd('colorscheme moonfly')
 vim.cmd('syntax on')
 
 if (vim.g.colors_name == 'moonfly') then
     vim.api.nvim_exec([[
         hi Normal guibg=NONE
-        hi StatusLine guibg=#080808
-        hi TabLineFill guibg=#080808
-        hi TabLine guibg=#080808
-        hi TabLineSel guibg=#080808
+        hi StatusLine guibg=NONE guifg=#b2b2b2 gui=bold
+        hi TabLineFill guibg=NONE
+        hi TabLine guibg=NONE
+        hi TabLineSel guibg=NONE
     ]], true)
 elseif (vim.g.colors_name == 'gruvbox') then
     vim.g.gruvbox_contrast_dark='hard'
