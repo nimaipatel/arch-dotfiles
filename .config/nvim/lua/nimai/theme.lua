78644ce656aad886f445b72330867bbfd551bc5d
nimai <nimai.m.patel@gmail.com>
2021-01-26 16:50:19 +0530

    gruvbox

--- .config/nvim/lua/nimai/theme.lua
+++ .config/nvim/lua/nimai/theme.lua
@@ -1,23 +1,32 @@
 vim.o.termguicolors = true
 vim.o.background = 'dark'
-vim.cmd('colorscheme moonfly')
+require("colorbuddy").colorscheme("gruvbox")
 vim.cmd('syntax on')
 
 if (vim.g.colors_name == 'moonfly') then
     vim.api.nvim_exec([[
         hi Normal guibg=NONE
         hi StatusLine guibg=NONE guifg=#b2b2b2 gui=bold
-        hi TabLineFill guibg=NONE
-        hi TabLine guibg=NONE
-        hi TabLineSel guibg=NONE
+        hi TabLineFill guibg=NONE gui=bold
+        hi TabLine guibg=NONE gui=bold
+        hi TabLineSel guibg=NONE gui=bold
     ]], true)
 elseif (vim.g.colors_name == 'gruvbox') then
     vim.g.gruvbox_contrast_dark='hard'
     vim.api.nvim_exec([[
         hi Normal guibg=NONE
         hi StatusLine guibg=NONE guifg=#ebdbb2 gui=bold
-        hi TabLineFill guibg=NONE
-        hi TabLine guibg=NONE
-        hi TabLineSel guibg=NONE
+        hi TabLineFill guibg=NONE gui=bold
+        hi TabLine guibg=NONE gui=bold
+        hi TabLineSel guibg=NONE gui=bold
+    ]], true)
+elseif (vim.g.colors_name == 'dracula') then
+    vim.g.gruvbox_contrast_dark='hard'
+    vim.api.nvim_exec([[
+        hi Normal guibg=NONE
+        hi StatusLine guibg=NONE guifg=#f8f8f2 gui=bold
+        hi TabLineFill guibg=NONE gui=bold
+        hi TabLine guibg=NONE gui=bold
+        hi TabLineSel guibg=NONE gui=bold
     ]], true)
 end
