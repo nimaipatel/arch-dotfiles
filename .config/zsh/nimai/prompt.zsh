14ccdc2996a84d4526a59efde4199d3bd0a63ea4
nimai <nimai.m.patel@gmail.com>
2021-02-02 18:01:26 +0530

    prompt char

--- .config/zsh/nimai/prompt.zsh
+++ .config/zsh/nimai/prompt.zsh
@@ -7,4 +7,4 @@ zstyle ':vcs_info:git:*' formats ' on branch %b'
  
 # Set up the prompt (with git branch name)
 setopt PROMPT_SUBST
-PROMPT='%F{5}${PWD/#$HOME/~}%f%F{6}${vcs_info_msg_0_}%f %F{3}$%f '
+PROMPT='%F{5}${PWD/#$HOME/~}%f%F{6}${vcs_info_msg_0_}%f %F{3}ॐ %f '
