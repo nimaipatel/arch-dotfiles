f880f966036d848eb8b69f5f0dbc36463fb85167
nimai <nimai.m.patel@gmail.com>
2021-02-21 13:55:03 +0530

    use gruvbox8 to increase startup time 😼

--- .config/nvim/lua/nimai/theme.lua
+++ .config/nvim/lua/nimai/theme.lua
@@ -1,32 +1,4 @@
 vim.o.termguicolors = true
 vim.o.background = 'dark'
-require('colorbuddy').colorscheme('gruvbox')
+vim.cmd('colorscheme gruvbox8_hard')
 vim.cmd('syntax on')
-
-if (vim.g.colors_name == 'moonfly') then
-    vim.api.nvim_exec([[
-        hi Normal guibg=NONE
-        hi StatusLine guibg=NONE guifg=#b2b2b2 gui=bold
-        hi TabLineFill guibg=NONE gui=bold
-        hi TabLine guibg=NONE gui=bold
-        hi TabLineSel guibg=NONE gui=bold
-    ]], true)
-elseif (vim.g.colors_name == 'gruvbox') then
-    vim.g.gruvbox_contrast_dark='hard'
-    vim.api.nvim_exec([[
-        hi Normal guibg=NONE
-        hi StatusLine guibg=NONE guifg=#ebdbb2 gui=bold
-        hi TabLineFill guibg=NONE gui=bold
-        hi TabLine guibg=NONE gui=bold
-        hi TabLineSel guibg=NONE gui=bold
-    ]], true)
-elseif (vim.g.colors_name == 'dracula') then
-    vim.g.gruvbox_contrast_dark='hard'
-    vim.api.nvim_exec([[
-        hi Normal guibg=NONE
-        hi StatusLine guibg=NONE guifg=#f8f8f2 gui=bold
-        hi TabLineFill guibg=NONE gui=bold
-        hi TabLine guibg=NONE gui=bold
-        hi TabLineSel guibg=NONE gui=bold
-    ]], true)
-end
