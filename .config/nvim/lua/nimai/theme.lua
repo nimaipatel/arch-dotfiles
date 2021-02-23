efef0581d5bed7879264e6547aca71c561e9cb00
nimai <nimai.m.patel@gmail.com>
2021-02-23 16:16:17 +0530

    minimal

--- .config/nvim/lua/nimai/theme.lua
+++ .config/nvim/lua/nimai/theme.lua
@@ -2,3 +2,10 @@ vim.o.termguicolors = true
 vim.o.background = 'dark'
 vim.cmd('colorscheme gruvbox8_hard')
 vim.cmd('syntax on')
+vim.api.nvim_exec([[
+    hi Normal guibg=NONE
+    hi StatusLine guibg=NONE guifg=#ebdbb2 gui=bold
+    hi TabLineFill guibg=NONE gui=bold
+    hi TabLine guibg=NONE gui=bold
+    hi TabLineSel guibg=NONE gui=bold
+]], true)
