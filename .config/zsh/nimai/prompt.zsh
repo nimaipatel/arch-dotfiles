60b60275a5fe62277f759bf9dba8abebcfa33db3
nimai <nimai.m.patel@gmail.com>
2021-03-10 10:13:01 +0530

    Truncate path in zsh PS1 to show maximum 3 parent directories of current directory

--- .config/zsh/nimai/prompt.zsh
+++ .config/zsh/nimai/prompt.zsh
@@ -7,4 +7,4 @@ zstyle ':vcs_info:git:*' formats ' on branch %b'
  
 # Set up the prompt (with git branch name)
 setopt PROMPT_SUBST
-PROMPT='%F{1}[%f%F{2}%n$f %F{4}at %m%f %F{5}in ${PWD/#$HOME/~}%f%F{6}${vcs_info_msg_0_}%f%F{1}]%f%F{3}$%f '
+PROMPT='%F{1}[%f%F{2}%n$f %F{4}at %m%f %F{5}in %(5~|%-1~/.../%3~|%4~)%f%F{6}${vcs_info_msg_0_}%f%F{1}]%f%F{3}$%f '
