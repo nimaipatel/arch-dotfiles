c7e5e04e4ed0807c9d7b0517c86e7a7bcd0fd893
nimai <nimai.m.patel@gmail.com>
2021-03-10 10:12:40 +0530

    Added more options for managing history

--- .config/zsh/nimai/options.zsh
+++ .config/zsh/nimai/options.zsh
@@ -1,16 +1,40 @@
 # include hidden files in tab autocomplete
 setopt GLOB_DOTS
 
+# History in cache directory:
+HISTSIZE=10000000
+SAVEHIST=10000000
+
+# Write to the history file immediately, not when the shell exits.
+setopt INC_APPEND_HISTORY
+
+# Share history between all sessions.
+setopt SHARE_HISTORY
+
+# Do not record an event that was just recorded again.
+setopt HIST_IGNORE_DUPS
+
+# Delete an old recorded event if a new event is a duplicate.
+setopt HIST_IGNORE_ALL_DUPS
+
+# Do not display a previously found event.
+setopt HIST_FIND_NO_DUPS
+
+# Do not record an event starting with a space.
+setopt HIST_IGNORE_SPACE
+
+# Do not write a duplicate event to the history file.
+setopt HIST_SAVE_NO_DUPS
+
+# append to history file
+setopt APPEND_HISTORY
+
 # highlight selected option in tab completion
 autoload -Uz compinit && compinit
 zstyle ':completion:*' menu select
 
 # case-insensitive, partial-word and then substring completion
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-
-# History in cache directory:
-HISTSIZE=10000000
-SAVEHIST=10000000
 
 function man() {
 	env \
