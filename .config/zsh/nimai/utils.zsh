df9582647f2716f461b4666dc721449fd2ec0185
nimai <nimai.m.patel@gmail.com>
2021-03-15 09:37:27 +0530

    added file/directory finder widget to fzf

--- .config/zsh/nimai/utils.zsh
+++ .config/zsh/nimai/utils.zsh
@@ -102,3 +102,27 @@ fzf-history-widget() {
 }
 zle     -N   fzf-history-widget
 bindkey '^R' fzf-history-widget
+
+# CTRL-T - Paste the selected file path(s) into the command line
+__fsel() {
+    local cmd="${FZF_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+-o -type f -print \
+-o -type d -print \
+-o -type l -print 2> /dev/null | cut -b3-"}"
+    setopt localoptions pipefail no_aliases 2> /dev/null
+    eval "$cmd" | fzf --height=40% -m "$@" | while read item; do
+        echo -n "${(q)item} "
+    done
+    local ret=$?
+    echo
+    return $ret
+}
+
+fzf-file-widget() {
+    LBUFFER="${LBUFFER}$(__fsel)"
+    local ret=$?
+    zle reset-prompt
+    return $ret
+}
+zle     -N   fzf-file-widget
+bindkey '^T' fzf-file-widget
