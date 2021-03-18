b5c969c17795b87bb6820b6044b21479e6611821
nimai <nimai.m.patel@gmail.com>
2021-03-18 11:39:52 +0530

    Function to remove trailing whitespace from file

--- .config/nvim/lua/nimai/commands.lua
+++ .config/nvim/lua/nimai/commands.lua
@@ -4,4 +4,5 @@ vim.api.nvim_exec([[
     command! -nargs=0 Gb :!git branch --show-current
     command! -nargs=0 Conf :cd $XDG_CONFIG_HOME/nvim | e $XDG_CONFIG_HOME/nvim/init.lua
     command! -nargs=0 CD :cd %:h | cd `git rev-parse --show-toplevel`
+    command! -nargs=0 StripTrailingWhiteSpace :%s/\s\+$//g
 ]], true)
