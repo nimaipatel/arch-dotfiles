5e7886199546904a92f37a6099e9787085d0d4fc
nimai <nimai.m.patel@gmail.com>
2021-03-26 19:20:40 +0530

    😎 script to change system wide colors mode

--- .config/nvim/lua/nimai/theme.lua
+++ .config/nvim/lua/nimai/theme.lua
@@ -1,11 +1,26 @@
 vim.o.termguicolors = true
-vim.o.background = 'dark'
-vim.cmd('colorscheme gruvbox8_hard')
 vim.cmd('syntax on')
-vim.api.nvim_exec([[
-    hi Normal guibg=NONE
-    hi StatusLine guibg=NONE guifg=#ebdbb2 gui=bold
-    hi TabLineFill guibg=NONE gui=bold
-    hi TabLine guibg=NONE gui=bold
-    hi TabLineSel guibg=NONE gui=bold
-]], true)
+
+function SetColorScheme(colorscheme)
+	if colorscheme == 'gruvbox_dark' then
+		vim.cmd('colorscheme gruvbox8_hard')
+		vim.o.background = 'dark'
+		vim.api.nvim_exec([[
+		    hi StatusLine guibg=NONE guifg=#ebdbb2 gui=bold
+		    hi TabLineFill guibg=NONE gui=bold
+		    hi TabLine guibg=NONE gui=bold
+		    hi TabLineSel guibg=NONE gui=bold
+		]], true)
+	elseif colorscheme == 'gruvbox_light' then
+		vim.cmd('colorscheme gruvbox8_hard')
+		vim.o.background = 'light'
+		vim.api.nvim_exec([[
+		    hi StatusLine guibg=NONE guifg=#3c3836 gui=bold
+		    hi TabLineFill guibg=NONE gui=bold
+		    hi TabLine guibg=NONE gui=bold
+		    hi TabLineSel guibg=NONE gui=bold
+		]], true)
+	end
+end
+
+SetColorScheme('gruvbox_dark')
