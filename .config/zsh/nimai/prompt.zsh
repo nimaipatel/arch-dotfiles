283e179a5c668fb4682c65ac8982a9216afa9f00
nimai <nimai.m.patel@gmail.com>
2021-04-22 20:48:45 +0530

    Shorten prompt

--- .config/zsh/nimai/prompt.zsh
+++ .config/zsh/nimai/prompt.zsh
@@ -3,8 +3,8 @@ autoload -Uz vcs_info
 precmd() { vcs_info }
 
 # Format the vcs_info_msg_0_ variable
-zstyle ':vcs_info:git:*' formats ' on branch %b'
+zstyle ':vcs_info:git:*' formats ' branch %b'
  
 # Set up the prompt (with git branch name)
 setopt PROMPT_SUBST
-PROMPT='%F{1}[%f%F{2}%n$f %F{4}at %m%f %F{5}in %(5~|%-1~/.../%3~|%4~)%f%F{6}${vcs_info_msg_0_}%f%F{1}]%f%F{3}$%f '
+PROMPT='%F{1}[%f%F{2}%n$f%F{8}@%f%F{4}%m%f %F{5}%(5~|%-1~/.../%3~|%4~)%f%F{6}${vcs_info_msg_0_}%f%F{1}]%f%F{3}$%f '
