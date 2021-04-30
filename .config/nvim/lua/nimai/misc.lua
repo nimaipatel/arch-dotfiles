ca1866a1d6b9ea52c81447e4d58c14ad4562337d
nimai <nimai.m.patel@gmail.com>
2021-04-30 11:37:49 +0530

    scrolloff fraction

--- .config/nvim/lua/nimai/misc.lua
+++ .config/nvim/lua/nimai/misc.lua
@@ -1,3 +1,8 @@
+AddEventListener('ScrolloffFraction', { 'BufEnter,WinEnter,WinNew,VimResized *,*.*' }, function ()
+	local vis_lines = vim.api.nvim_win_get_height(vim.fn.win_getid())
+	vim.o.scrolloff = math.floor(vis_lines * 0.25)
+end)
+
 AddEventListener('LuaHighlight', { 'TextYankPost *' }, function()
 	require'vim.highlight'.on_yank()
 end)
