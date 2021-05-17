80590a320c8ec95d3f6c836c13c4b8607e138ef5
nimai <nimai.m.patel@gmail.com>
2021-05-17 14:59:46 +0530

    Use GNU readline for language interpreters

--- .config/zsh/nimai/aliases.zsh
+++ .config/zsh/nimai/aliases.zsh
@@ -16,3 +16,7 @@ alias diff="diff --color"
 alias grep="grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}"
 alias wget="wget --hsts-file=\${XDG_CONFIG_HOME}/wget/wget-hsts"
 alias vi="nvim"
+
+# wrap interpreters in rlwrap to use inputrc settings
+alias node="NODE_NO_READLINE=1 rlwrap -a node"
+alias ghci="rlwrap -a ghci"
