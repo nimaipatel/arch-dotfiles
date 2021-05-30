e62dedb3a3cbeb81d80a0003ed1fa80e01fd0056
nimai <nimai.m.patel@gmail.com>
2021-05-30 22:10:03 +0530

    New prompt

--- .config/zsh/nimai/prompt.zsh
+++ .config/zsh/nimai/prompt.zsh
@@ -3,8 +3,8 @@ autoload -Uz vcs_info
 precmd() { vcs_info ; }
 
 # Format the vcs_info_msg_0_ variable
-zstyle ':vcs_info:git:*' formats ' branch %b'
+zstyle ':vcs_info:git:*' formats ' %b'
  
 # Set up the prompt (with git branch name)
 setopt PROMPT_SUBST
-PROMPT='%B%F{1}[%f%F{2}%n$f%F{8}@%f%F{4}%m%f %F{5}%(5~|%-1~/.../%3~|%4~)%f%F{6}${vcs_info_msg_0_}%f%F{1}]%f%F{3}$%f %b'
+PROMPT='%B%F{2}%n$f%F{8}@%f%F{4}%m%f %F{5}%(5~|%-1~/.../%3~|%4~)%f%F{6}${vcs_info_msg_0_}%f%F{3} >=>%f %b'
