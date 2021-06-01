f69446619aed0f6aeb31939699ac16597fbfaa01
nimai <nimai.m.patel@gmail.com>
2021-06-01 17:19:55 +0530

    Two space indentation for haskell instead of four

--- .config/nvim/lua/nimai/ftplugin.lua
+++ .config/nvim/lua/nimai/ftplugin.lua
@@ -1,3 +1,4 @@
 vim.api.nvim_exec([[
-	autocmd FileType html,htmldjango,xml,xsd,dtd,xslt,css,python,javascript,haskell setlocal shiftwidth=4 softtabstop=4 softtabstop=4 expandtab
+	autocmd FileType html,htmldjango,xml,xsd,dtd,xslt,css,python,javascriptsetlocal shiftwidth=4 softtabstop=4 softtabstop=4 expandtab
+	autocmd FileType haskell setlocal shiftwidth=2 softtabstop=2 softtabstop=2 expandtab
 ]],true)
