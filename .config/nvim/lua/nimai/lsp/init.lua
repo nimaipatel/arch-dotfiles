79e4cf2afd4ba5741a931b76b76db69c7b85334c
nimai <nimai.m.patel@gmail.com>
2021-06-09 18:04:30 +0530

    Redundant

--- .config/nvim/lua/nimai/lsp/init.lua
+++ .config/nvim/lua/nimai/lsp/init.lua
@@ -1,6 +1,3 @@
-vim.g.closetag_filetypes='html,xhtml,jsx,javascript,php' -- not lsp but whatever
-vim.g.completion_enable_auto_paren = 1
-vim.g.completion_enable_snippet = 'vim-vsnip'
 vim.g.completion_matching_smart_case = 1
 vim.g.completion_matching_strategy_list = {'fuzzy', 'substring', 'exact'}
 require('nimai.lsp.completion')
