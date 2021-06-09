d0cab76f54e648e9443919cccf4522f86495a0db
nimai <nimai.m.patel@gmail.com>
2021-06-09 15:43:26 +0530

    Shift to snap from telescope for fuzzy file finding and live grep

--- .config/nvim/lua/nimai/mappings.lua
+++ .config/nvim/lua/nimai/mappings.lua
@@ -17,14 +17,36 @@ nnoremap('<leader><cr>', ':nohl<cr>')
 -- when jumping to bottom, center the page
 nnoremap('G', 'Gzz')
 
--- switch buffers quickly
-nnoremap('<leader><leader>', ':buffers<cr>:buffer ')
-
 -- spawn zsh shell with control + t
 nnoremap('<c-t>', ':8sp term://zsh <cr>')
 -- turn terminal to normal mode with escape
 nnoremap('<esc>', '<c-\\><c-n>')
 
-nnoremap('<C-p>', '<cmd>Telescope find_files<CR>')
-nnoremap('<space>g', '<cmd>Telescope live_grep<CR>')
-nnoremap('<space>b', '<cmd>Telescope buffers<CR>')
+local snap = require('snap')
+
+snap.register.map({"n"}, {"<c-p>"}, function ()
+	snap.run {
+		producer = snap.get('consumer.fzf')(snap.get('producer.ripgrep.file')),
+		select = snap.get('select.file').select,
+		multiselect = snap.get('select.file').multiselect,
+		views = {snap.get('preview.file')}
+	}
+end)
+
+snap.register.map({"n"}, {"<c-f>"}, function ()
+	snap.run {
+		producer = snap.get('producer.ripgrep.vimgrep'),
+		select = snap.get('select.vimgrep').select,
+		multiselect = snap.get('select.vimgrep').multiselect,
+		views = {snap.get('preview.vimgrep')}
+	}
+end)
+
+snap.register.map({"n"}, {"<leader><leader>"}, function ()
+	snap.run {
+		producer = snap.get('consumer.fzf')(snap.get('producer.vim.buffer')),
+		select = snap.get('select.file').select,
+		multiselect = snap.get('select.file').multiselect,
+		views = {snap.get('preview.file')}
+	}
+end)
