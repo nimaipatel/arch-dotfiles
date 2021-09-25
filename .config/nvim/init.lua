bc76960dbd71018432ebf77cc3124567ae4b1644
nimai <nimai.m.patel@gmail.com>
2021-09-25 16:35:56 +0530

    Remove comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -434,11 +434,11 @@ packer.startup(function(use)
         config = function()
             require('nvim-autopairs').setup {}
             require('nvim-autopairs.completion.cmp').setup {
-                map_cr = true, --  map <CR> on insert mode
-                map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-                auto_select = true, -- automatically select the first item
-                insert = false, -- use insert confirm behavior instead of replace
-                map_char = { -- modifies the function or method delimiter by filetypes
+                map_cr = true,
+                map_complete = true,
+                auto_select = true,
+                insert = false,
+                map_char = {
                     all = '(',
                     tex = '{',
                 },
