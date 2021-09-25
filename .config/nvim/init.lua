745fe839fe63f4a4ee0998ba0bffd63d76e4846b
nimai <nimai.m.patel@gmail.com>
2021-09-25 10:07:59 +0530

    Configure auto pairs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -406,4 +406,22 @@ packer.startup(function(use)
             RefreshColors()
         end,
     }
+
+    use {
+        'windwp/nvim-autopairs',
+        requires = 'hrsh7th/nvim-cmp',
+        config = function()
+            require('nvim-autopairs').setup {}
+            require('nvim-autopairs.completion.cmp').setup {
+                map_cr = true, --  map <CR> on insert mode
+                map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
+                auto_select = true, -- automatically select the first item
+                insert = false, -- use insert confirm behavior instead of replace
+                map_char = { -- modifies the function or method delimiter by filetypes
+                    all = '(',
+                    tex = '{',
+                },
+            }
+        end,
+    }
 end)
