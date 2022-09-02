11fd84d8e88d0a4058d1d5839b0753bddca0da0f
nimai <nimai.m.patel@gmail.com>
2022-09-02 16:34:43 +0530

    spotify widget

--- Awesome/.config/awesome/rc.lua
+++ Awesome/.config/awesome/rc.lua
@@ -163,6 +163,103 @@ end
 
 screen.connect_signal('property::geometry', set_wallpaper)
 
+local spotify_widget = wibox.widget {
+    {
+        id = 'artist',
+        font = beautiful.font,
+        widget = wibox.widget.textbox,
+        align = 'right',
+        forced_width = 300,
+    },
+    {
+        id = 'icon',
+        widget = wibox.widget.imagebox,
+    },
+    {
+        layout = wibox.container.scroll.horizontal,
+        max_size = 300,
+        forced_width = 300,
+        step_function = wibox.container.scroll.step_functions.waiting_nonlinear_back_and_forth,
+        speed = 40,
+        {
+            id = 'title',
+            font = beautiful.font,
+            widget = wibox.widget.textbox,
+        },
+    },
+    layout = wibox.layout.fixed.horizontal,
+    spacing = 10,
+}
+
+spotify_widget:connect_signal('button::press', function(_, _, _, button)
+    if button == 1 then
+        awful.spawn('playerctl --player=spotify play-pause', false) -- left click
+    elseif button == 4 then
+        awful.spawn('playerctl --player=spotify next', false) -- scroll up
+    elseif button == 5 then
+        awful.spawn('playerctl --player=spotify previous', false) -- scroll down
+    end
+end)
+
+local play_icon = '/usr/share/icons/Papirus-Light/24x24/categories/spotify.svg'
+local pause_icon = '/usr/share/icons/Papirus-Dark/24x24/panel/spotify-indicator.svg'
+
+local _spotify_icon = awful.widget.watch( --luacheck: ignore
+    [[playerctl --player=spotify status]],
+    1,
+    function(widget, stdout, stderr, exitreason, exitcode) --luacheck: ignore
+        if exitcode ~= 0 then
+            spotify_widget:get_children_by_id('icon')[1]:set_visible(false)
+            return
+        end
+        spotify_widget:get_children_by_id('icon')[1]:set_visible(true)
+        local line = stdout:match '[^\r\n]+'
+        if line == 'Playing' then
+            spotify_widget:get_children_by_id('icon')[1]:set_image(play_icon)
+        elseif line == 'Paused' then
+            spotify_widget:get_children_by_id('icon')[1]:set_image(pause_icon)
+        end
+    end
+)
+
+local _spotify_artist_title = awful.widget.watch( --luacheck: ignore
+    [[playerctl --player=spotify metadata]],
+    1,
+    function(widget, stdout, stderr, exitreason, exitcode) --luacheck: ignore
+        if exitcode ~= 0 then
+            spotify_widget:get_children_by_id('artist')[1]:set_markup ''
+            spotify_widget:get_children_by_id('title')[1]:set_markup ''
+            return
+        end
+        local artist, title
+        for line in stdout:gmatch '[^\r\n]+' do
+            local _
+            if line:match 'spotify xesam:artist' then
+                _, artist = line:match 'spotify xesam:artist( +)(.+)'
+            end
+            if line:match 'spotify xesam:title' then
+                _, title = line:match 'spotify xesam:title( +)(.+)'
+            end
+        end
+        spotify_widget:get_children_by_id('artist')[1]:set_markup(artist)
+        spotify_widget:get_children_by_id('title')[1]:set_markup(title)
+    end
+)
+
+local wifi_widget = awful.widget.watch(
+    [[nmcli -get-values IN-USE,SSID,SIGNAL device wifi list]],
+    5,
+    function(widget, stdout)
+        widget:set_text 'no internet'
+        for line in stdout:gmatch '[^\r\n]+' do
+            if line:match '^*:' then
+                local ssid, power = line:match [[^*:(.+):(.+)]]
+                widget:set_text('ssid: ' .. ssid .. ' power: ' .. power .. '%')
+            end
+        end
+    end
+)
+
 awful.screen.connect_for_each_screen(function(s)
     -- Each screen has its own tag table.
     awful.tag(
@@ -318,10 +415,15 @@ awful.screen.connect_for_each_screen(function(s)
             },
         },
         { -- Right widgets
-            layout = wibox.layout.fixed.horizontal,
+            valign = 'center',
+            halign = 'center',
+            layout = wibox.container.place,
+            spotify_widget,
         },
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
+            spacing = dpi(10),
+            wifi_widget,
             lain.widget.bat {
                 timeout = 1,
                 battery = 'BAT1',
