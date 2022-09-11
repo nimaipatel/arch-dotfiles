94d72469bc21ff224609068d71efcda60c95cb3d
nimai <nimai.m.patel@gmail.com>
2022-09-11 18:41:26 +0530

    lol

--- Awesome/.config/awesome/rc.lua
+++ Awesome/.config/awesome/rc.lua
@@ -12,7 +12,6 @@ local cpu_bars_widget = require 'widgets.cpu_bars'
 local memory_widget = require 'widgets.memory'
 local battery_widget = require 'widgets.battery'
 local pipewire_widget = require 'widgets.pipewire'
-local spotify_widget = require 'widgets.spotify'
 local util = require 'util'
 
 -- Standard awesome library
@@ -286,7 +285,6 @@ for s in screen do
             valign = 'center',
             halign = 'center',
             layout = wibox.container.place,
-            spotify_widget,
         },
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
@@ -334,6 +332,18 @@ local gkeep = lain.util.quake {
 
 -- {{{ Key bindings
 local globalkeys = gears.table.join(
+    key({ modkey }, 'v', function()
+        for _, c in ipairs(client.get()) do
+            if c.class:match 'kitty' and c.name:match 'nvim' then
+                c:jump_to()
+                return
+            end
+        end
+        awful.spawn('kitty -e nvim', {
+            tag = ' ૧ ',
+        })
+    end),
+
     key({ modkey }, 'b', function()
         for _, c in ipairs(client.get()) do
             if c.class:match '^Brave%-browser' then
@@ -342,7 +352,9 @@ local globalkeys = gears.table.join(
             end
         end
         -- urgent tags are automatically focused in this config
-        awful.util.spawn 'brave'
+        awful.spawn('brave', {
+            tag = ' ૨ ',
+        })
     end),
 
     key({ modkey }, '=', function()
