ae50e8c8d1c1ddabbcee73c2e9ac9793fcc6cf07
nimai <nimai.m.patel@gmail.com>
2022-10-01 13:20:04 +0530

    nice fish

---
+++ Fish/.bash_profile
@@ -0,0 +1,39 @@
+# default executables
+export EDITOR="nvim"
+export VISUAL="nvim"
+export TERMINAL="kitty"
+export BROWSER="brave"
+export READER="zathura"
+
+# fix window-frames in vieb browser
+export VIEB_WINDOW_FRAME=true
+
+# fix white screen of death in java awt programs
+export _JAVA_AWT_WM_NONREPARENTING=1
+
+# my scripts
+export MY_SCRIPTS="$HOME/.local/bin/my_scripts"
+export MY_SCRIPT_ASSETS="$HOME/.local/share/my_script_assets"
+
+# en_IN doesn't work as expected
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+# ~/ Clean-up:
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+export ANDROID_SDK_ROOT="/opt/android-sdk"
+export ANDROID_HOME="/opt/android-sdk"
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk/
+
+# PATH variable
+export PATH="${HOME}/.bin:${PATH}"
+export PATH="${HOME}/.local/bin:${PATH}"
+export PATH="${HOME}/.local/share/bin:${PATH}"
+export PATH="$ANDROID_HOME/emulator:$PATH"
+export PATH="$JAVA_HOME/bin:$PATH"
+export PATH="$HOME/.ghcup/bin:$PATH"
+
+# this is always used by fzf
+export FZF_DEFAULT_OPTS="--color=16 --padding=0,1 --cycle --prompt='>=> ' --pointer='>' --info=inline --layout=reverse"
--- Fish/.config/fish/config.fish
+++ Fish/.config/fish/config.fish
@@ -1,60 +1,17 @@
 #!/usr/bin/fish
 
-if status --is-login
-    set -x EDITOR nvim
-    set -x VISUAL nvim
-
-    set -x TERMINAL st
-    set -x BROWSER brave
-    set -x READER zathura
-    set -x TERMINAL_FILE_BROWSER ranger
-    set -x GUI_FILE_BROWSER pcmanfm
-
-    # fix window-frames in vieb browser
-    set -x VIEB_WINDOW_FRAME true
-
-    # fix white screen of death in java awt programs
-    set -x _JAVA_AWT_WM_NONREPARENTING 1
-
-    # my scripts
-    set -x MY_SCRIPTS $HOME/.local/bin/my_scripts
-    set -x MY_SCRIPT_ASSETS $HOME/.local/share/my_script_assets
-
-    # en_IN doesn't work as expected
-     set -x LC_ALL en_US.UTF-8
-     set -x LANG en_US.UTF-8
-
-    # ~/ clean up (I don't actually care):
-     set -x XDG_CONFIG_HOME $HOME/.config
-     set -x XDG_DATA_HOME $HOME/.local/share
-     set -x XDG_CACHE_HOME $HOME/.cache
-     set -x ANDROID_SDK_ROOT /opt/android-sdk
-     set -x ANDROID_HOME /opt/android-sdk
-     set -x JAVA_HOME /usr/lib/jvm/java-8-openjdk/
-
-    # PATH variable
-    fish_add_path $MY_SCRIPTS
-    fish_add_path $HOME/.local/bin
-    fish_add_path $HOME/.local/share/bin
-    fish_add_path $ANDROID_HOME/emulator
-    fish_add_path $JAVA_HOME/bin
-    fish_add_path $HOME/.ghcup/bin
-    fish_add_path $HOME/.cabal/bin
-    fish_add_path $HOME/.emacs.d/bin
-
-    # [ "$(tty)" = "/dev/tty1" ] && ! (pidof dwm) && startx
-end
+starship init fish | source
 
 alias mv "mv -iv"
 alias cp "cp -iv"
-alias rm "echo Are you sure\? 👉👈; false"
 alias ls "ls --color=tty"
 alias diff "diff --color"
 alias grep "grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}"
-alias vi "nvim"
-alias vim "nvim"
 
 alias sw "dwmswallow \$WINDOWID && "
+
+abbr -a vi "nvim"
+abbr -a vim "nvim"
 
 abbr -a sz "sw zathura"
 abbr -a sv "sw nsxiv"
@@ -80,14 +37,6 @@ abbr -a groot "cd \$(git rev-parse --show-toplevel)"
 abbr -a gl "git log"
 abbr -a glo "git log --pretty='oneline'"
 abbr -a glol "git log --graph --oneline --decorate"
-
-abbr -a cst "cfg status"
-abbr -a ca "cfg add"
-abbr -a cdf "cfg diff"
-abbr -a ccm "cfg commit -m"
-abbr -a cm "cfg commit"
-abbr -a cpom "cfg push origin master"
-abbr -a clol "cfg log --graph --oneline --decorate"
 
 abbr -a l "exa -a"
 abbr -a ll "exa -al"
@@ -131,51 +80,6 @@ function fish_hybrid_key_bindings --description \
     fish_vi_key_bindings --no-erase
 end
 set -g fish_key_bindings fish_hybrid_key_bindings
-
-# disable greeting
-set -U fish_greeting
-
-# disable vi-mode in prompt
-function fish_mode_prompt; end
-
-set __fish_git_prompt_color cyan
-set __fish_git_prompt_color_suffix yellow
-set __fish_git_prompt_color_prefix yellow
-function fish_prompt
-    if not set -q VIRTUAL_ENV_DISABLE_PROMPT
-        set -g VIRTUAL_ENV_DISABLE_PROMPT true
-    end
-    set_color -o green
-    printf '%s' $USER
-    set_color normal
-
-    set_color -o yellow
-    printf ' at '
-    set_color normal
-
-    set_color -o blue
-    echo -n (prompt_hostname)
-    set_color normal
-
-    set_color -o yellow
-    printf ' in '
-    set_color normal
-
-    set_color -o magenta
-    printf '%s' (prompt_pwd)
-    set_color normal
-
-    printf '%s' (fish_git_prompt)
-
-    echo
-    if test -n "$VIRTUAL_ENV"
-        printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
-    end
-
-    set_color -o yellow
-    printf '>=> '
-    set_color normal
-end
 
 set -x LESS_TERMCAP_mb (printf "\033[01;31m")
 set -x LESS_TERMCAP_md (printf "\033[01;31m")
